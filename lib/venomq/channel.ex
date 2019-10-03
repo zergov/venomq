5f49763dc31311cde0a10c38979aaced11ce66d3
Jonathan Lalande <lalandej.gg@gmail.com>
2019-10-03 10:53:53 -0400

    understands exchange declare command
    receives exchange.declare
    return exchange.declare_ok

--- lib/venomq/channel.ex
+++ lib/venomq/channel.ex
@@ -15,19 +15,29 @@ defmodule Venomq.Channel do
 
   def handle_continue(:initialize, state) do
     Logger.info("Spawned a channel with pid: #{inspect(self())}")
-    write_socket(state.socket, "channel.open_ok")
+    write_socket(state.socket, "channel.open_ok\n")
     {:noreply, state.socket}
   end
 
   def handle_info({:tcp, socket, data}, state) do
     Logger.info("#{inspect(self())} | message received: #{String.trim(data)}")
-    write_socket(socket, data)
+    parse_command(data, socket)
     {:noreply, state}
   end
 
   def handle_info({:tcp_closed, _socket}, _state) do
     Logger.info("#{inspect(self())} | connection closed.")
     Process.exit(self(), :normal)
+  end
+
+  defp parse_command("exchange.declare" <> _args, socket) do
+    Logger.info("declaring channel...")
+    write_socket(socket, "exchange.declare_ok\n")
+  end
+
+  defp parse_command(data, socket) do
+    Logger.info("cannot process message: #{data}")
+    write_socket(socket, "channel.not_found\n")
   end
 
   defp write_socket(socket, data) do
